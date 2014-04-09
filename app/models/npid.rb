425f384a805f7b0ecc16092ca9a59f44248d130b
Blessings <blessings.mhango@gmail.com>
2014-04-09 14:21:47 +0200

    built national id model

--- app/models/npid.rb
+++ app/models/npid.rb
@@ -1,2 +1,11 @@
+require 'couchrest_model'
+
 class Npid < CouchRest::Model::Base
+
+  property :national_id, String
+  property :site_code, String
+  property :assigned, TrueClass, :default => false
+  
+  timestamps!
+
 end
