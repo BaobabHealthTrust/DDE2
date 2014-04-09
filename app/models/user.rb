319be5f68797d21797a31b1247c4cb7188184ece
Blessings <blessings.mhango@gmail.com>
2014-04-09 14:19:38 +0200

    built user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
+require 'couchrest_model'
+
 class User < CouchRest::Model::Base
+
+  property :name, String
+  property :password_hash, String
+  property :email, String
+  property :description, String
+  property :disabled, TrueClass, :default => false
+  property :notify, TrueClass, :default => false
+  property :site_code, String
+  
+  timestamps!
+  
 end
