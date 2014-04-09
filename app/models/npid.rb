a7afe5251ee5f86b53c9ebbc2f563e12faea1ccd
Blessings <blessings.mhango@gmail.com>
2014-04-09 15:18:13 +0200

    added auto increment for npid

--- app/models/npid.rb
+++ app/models/npid.rb
@@ -2,10 +2,29 @@ require 'couchrest_model'
 
 class Npid < CouchRest::Model::Base
 
-  property :national_id, String
+  
+
+  def national_id
+    self['_id']
+  end
+
+  def self.last_id
+    previous_id = self.by__id.last.national_id
+    return previous_id
+  end
+
+  def national_id=(value) 
+       self['_id'] = (value.to_i + 1).to_s
+  end
+
   property :site_code, String
   property :assigned, TrueClass, :default => false
   
   timestamps!
+
+
+  design do
+    view :by__id
+  end
 
 end
