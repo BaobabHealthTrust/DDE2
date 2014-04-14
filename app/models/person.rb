f3ffd61d6533fbc217bb1fb93573b500f68821ea
Blessings <blessings.mhango@gmail.com>
2014-04-14 15:09:00 +0200

    building person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -12,9 +12,41 @@ class Person < CouchRest::Model::Base
 
   property :assigned_site, String
   property :patient_assigned, TrueClass, :default => false
+
+  property :attributes  do
+    property :citizenship, String
+    property :occupation, String
+    property :home_phone_number, String
+    property :cell_phone_number, String
+    property :race, String
+  end
+
   property :gender, String
+
+  property :names do
+     property :given_name, String
+     property :family_name, String 
+  end
+
+  property :patient do
+    property :identifiers do 
+     property :old_national_id,String
+     property :diabetes_number,String	
+    
+  end
+
   property :birthdate, String
   property :birthdate_estimated,  TrueClass, :default => false
+
+  property :addresses do
+    property :current_residence, String
+    property :current_village, String
+    property :current_ta, String
+    property :current_district, String
+    property :home_village, String
+    property :home_ta, String
+    property :home_district, String
+  end
 
   timestamps!
 
@@ -22,4 +54,5 @@ class Person < CouchRest::Model::Base
   design do
     view :by__id
   end
+
 end
