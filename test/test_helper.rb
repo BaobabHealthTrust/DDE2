fb97d21c666d4967c90c7b6bdfd8479b5fb61ab7
Chimwemwe <chimwemwe.kachaje@baobabhealth.org>
2014-04-23 16:54:40 +0200

    Added method in test_helper to load npids for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,4 +5,16 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   
+  j = 0
+  (1..50).collect{|n| n}.shuffle.each do |i|
+    id = ("XXX%03d" % i)
+    
+    Npid.find_by__id((j + 1).to_s).destroy rescue nil
+    
+    Npid.create(incremental_id: j, national_id: id, site_code: nil, assigned: false)
+    
+    j += 1
+    
+  end
+  
 end
