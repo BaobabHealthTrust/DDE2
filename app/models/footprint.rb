bb0fed55e0ae1b255c9c91c559bb0252225450f9
Chimwemwe <chimwemwe.kachaje@baobabhealth.org>
2014-04-23 09:50:02 +0200

    Added properties for Footprint model

--- app/models/footprint.rb
+++ app/models/footprint.rb
@@ -1,5 +1,11 @@
 class Footprint < CouchRest::Model::Base
   
+  property :npid, String
+  property :application, String
+  property :site_code, String
+  
+  timestamps!
+
 =begin
   + log_application_and_site(
       npid:String,
