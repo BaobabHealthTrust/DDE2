b9516e0888ed3c2bd0431f157db70839e907321c
Chimwemwe <chimwemwe.kachaje@baobabhealth.org>
2014-04-24 13:56:53 +0200

    Added views to RequestsQue model

--- app/models/requests_que.rb
+++ app/models/requests_que.rb
@@ -7,4 +7,16 @@ class RequestsQue < CouchRest::Model::Base
   
   timestamps!
 
+  design do
+    view :by__id
+    
+    view :pending,
+         :map => "function(doc){
+            if (doc['type'] == 'RequestsQue' && doc['request_processed'] == false && doc['region'] == '#{Site.current_region}'){
+                  emit(doc.site_code, {id: doc._id ,site_code: doc.site_code, 
+                      region: doc.region, threshold: doc.threshold, request_processed: doc.request_processed});
+            }
+          }"
+  end
+
 end
