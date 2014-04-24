1c6b1fff5ba95ba0a4c203167aedce2395c21177
Chimwemwe <chimwemwe.kachaje@baobabhealth.org>
2014-04-24 10:53:00 +0200

    Filtered out processing functions in footprint model to lib/utils

--- app/models/footprint.rb
+++ app/models/footprint.rb
@@ -6,17 +6,4 @@ class Footprint < CouchRest::Model::Base
   
   timestamps!
 
-=begin
-  + log_application_and_site(
-      npid:String,
-      application:String,
-      site:String
-    ):Array(JSON)
-  
-  
-=end
-  def self.log_application_and_site(npid, application, site)
-  
-  end
-   
 end
---
+++ lib/utils/footprint.rb
@@ -0,0 +1,20 @@
+module Utils
+
+  class Footprint
+    
+=begin
+  + log_application_and_site(
+      npid:String,
+      application:String,
+      site:String
+    ):Array(JSON)
+  
+  
+=end
+    def self.log_application_and_site(npid, application, site)
+    
+    end
+     
+  end
+
+end
