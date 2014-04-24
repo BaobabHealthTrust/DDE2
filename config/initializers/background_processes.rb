ed1f386b1beccbe10e11674c164fe2bffcea0830
Chimwemwe <chimwemwe.kachaje@baobabhealth.org>
2014-04-24 23:48:22 +0200

    Added background processes for npids assignments to sites

---
+++ config/initializers/background_processes.rb
@@ -0,0 +1,37 @@
+logger = Logger.new("#{Rails.root}/log/background_processes_thresholds.log")
+
+settings = YAML.load_file(Rails.root.join('config', 'couchdb.yml'))[Rails.env]
+
+logger.info("Running in '#{(!settings["mode"].nil? ? settings["mode"] : "undefined")}' mode")
+
+if !settings["mode"].nil? && Rails.env.downcase != "test"
+
+  if settings["mode"].to_s.strip.downcase == "master"
+
+    Thread.new do |t|
+      
+      while true
+        
+        logger.info("Checking thresholds")
+        
+        result = RestClient.get("http://localhost:3000/check_thresholds") rescue "nothing"
+        
+        logger.info("Received #{result}")
+        
+        sleep 10
+        
+        logger.info("Processing queues")
+        
+        result = RestClient.get("http://localhost:3000/process_queued_sites") rescue "nothing"
+        
+        logger.info("Received #{result}")
+        
+        sleep 10
+        
+      end
+
+    end
+  
+  end
+
+end
