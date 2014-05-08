ee9944541ef911aae81e64389f283b13b9c67fe6
Blessings <blessings.mhango@gmail.com>
2014-05-08 11:48:00 +0200

    added create footprint method

--- config/routes.rb
+++ config/routes.rb
@@ -77,7 +77,7 @@ Rails.application.routes.draw do
 
   resources :people do
     collection do
-      post :find,:create, :update, :destroy
+      post :find,:create, :update,:create_footprint
       get :index, :new, :show, :edit, :confirm_demographics
     end
   end
