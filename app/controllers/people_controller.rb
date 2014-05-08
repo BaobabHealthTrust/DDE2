637c6a68aad82d444fb1bc603cfc469ed4e23361
Blessings <blessings.mhango@gmail.com>
2014-05-08 11:45:37 +0200

    added create person method

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -17,6 +17,18 @@ class PeopleController < ApplicationController
   end
 
   def create
+    @person = Utils::PersonUtil.process_person_data(params.to_json)
+    @person_hash = JSON.parse(@person.to_json)
+    @npid_hash = {npid: {value: @person_hash["_id"]}}
+    @person_hash.merge!@npid_hash
+
+    if @person_hash
+        respond_to do |format|
+            format.html { redirect_to(@person_hash, :notice => 'Person was successfully created.') }
+            format.xml  { render :xml  => @person_hash, :status => :created, :location => @person_hash }
+            format.json { render :json => @person_hash, :status => :created, :location => @person_hash }
+        end
+      end    
   end
 
   def edit
