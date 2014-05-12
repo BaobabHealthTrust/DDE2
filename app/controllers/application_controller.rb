49d09e66ce82c22d4f9f2202f52e626d4500df3d
Blessings <blessings.mhango@gmail.com>
2014-05-12 12:05:04 +0200

    added cancancan methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,24 +6,59 @@ class ApplicationController < ActionController::Base
 
   skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
                                                                                 
-  #before_filter :perform_basic_auth, :except => ['login','logout','find','create']                         
-  before_filter :perform_basic 
+  before_filter :perform_basic_auth
+
+  rescue_from CanCan::AccessDenied,
+      :with => :access_denied
+
+  helper_method :current_user
+                    
   protected                                                                     
                               
-  def perform_basic
-    if not session[:user_id].blank?                                          
-      User.current = User.find session[:user_id]
-    end                                                                         
+  def login!(user)
+    session[:current_user_id] = user.id
+    @current_user = user
   end
-                                                                                
-  def perform_basic_auth                                                        
-    if session[:user_id].blank?                                                 
-      respond_to do |format|                                                    
-        format.html { redirect_to :controller => 'user',:action => 'logout' }   
-      end                                                                       
-    elsif not session[:user_id].blank?                                          
-      User.current = User.find session[:user_id]
-    end                                                                         
-  end 
+
+  def logout!
+    session[:current_user_id] = nil
+    @current_user = nil
+  end
+
+  def current_user
+    unless @current_user == false # meaning a user has previously been established as not logged in
+      @current_user ||= authenticate_from_session || authenticate_from_basic_auth || false
+      User.current_user = @current_user
+    end
+  end
+
+  def authenticate_from_basic_auth
+    authenticate_with_http_basic do |user_name, password|
+      user = Utils::UserUtil.get_active_user(user_name)
+      if user and user.password_matches?(password)
+        return user
+      else
+        return false
+      end
+    end
+  end
+
+  def authenticate_from_session
+    unless session[:current_user_id].blank?
+      user = Utils::UserUtil.get_active_user(session[:current_user_id])
+      return user
+    end
+  end
+
+  def perform_basic_auth
+    authorize! :access, :anything
+  end
+
+  def access_denied
+    respond_to do |format|
+      format.html { redirect_to login_path(referrer_param => current_path) }
+      format.any  { head :unauthorized }
+    end
+  end
 
 end
