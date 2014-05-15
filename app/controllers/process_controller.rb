d5be4dc6569551eb8cbb7a8229feb1012b0e8d9d
Chimwemwe <chimwemwe.kachaje@baobabhealth.org>
2014-05-15 09:58:06 +0200

    Disabled test data in process controller

--- app/controllers/process_controller.rb
+++ app/controllers/process_controller.rb
@@ -1,7 +1,7 @@
 class ProcessController < ApplicationController
 
   def process_data
-    if Rails.env == "development" and true
+    if Rails.env == "development" and false
     
       params[:person] = "{\"national_id\":\"\",\"application\":\"test\",\"site_code\":\"TST\",\"names\":{\"family_name\":\"Test\",\"given_name\":\"Test\"},\"gender\":\"M\",\"attributes\":{\"occupation\":\"\",\"cell_phone_number\":\"\"},\"birthdate\":\"2000-01-01\",\"patient\":{\"identifiers\":[\"P1700000011\",\"KCH20140503083423\",\"000000\"]},\"birthdate_estimated\":0,\"addresses\":{\"current_residence\":null,\"current_village\":null,\"current_ta\":null,\"current_district\":null,\"home_village\":null,\"home_ta\":null,\"home_district\":null}}"
       
@@ -32,7 +32,7 @@ class ProcessController < ApplicationController
   end
 
   def search
-    if Rails.env == "development" and true
+    if Rails.env == "development" and false
     
       params[:person] = "{\"national_id\":\"P1700000011\",\"application\":\"test\",\"site_code\":\"TST\",\"names\":{\"family_name\":\"Test\",\"given_name\":\"Test\"},\"gender\":\"M\",\"attributes\":{\"occupation\":\"\",\"cell_phone_number\":\"\"},\"birthdate\":\"2000-01-01\",\"patient\":{\"identifiers\":[\"P1700000011\",\"KCH20140503083423\"]},\"birthdate_estimated\":0,\"addresses\":{\"current_residence\":null,\"current_village\":null,\"current_ta\":null,\"current_district\":null,\"home_village\":null,\"home_ta\":null,\"home_district\":null}}"
       
