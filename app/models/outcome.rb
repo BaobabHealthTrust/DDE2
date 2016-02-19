800ae116ba43785b9893bded65e43f2cab4bbe42
Blessings <blessings.mhango@gmail.com>
2016-02-19 15:45:24 +0200

    updated outcome model

--- app/models/outcome.rb
+++ app/models/outcome.rb
@@ -4,6 +4,18 @@ class Outcome < CouchRest::Model::Base
 
  use_database "outcome"
  
+ property :person, String
+ property :outcome, String 
+ 
  timestamps!
+ 
+ design do 
+     view :by__id
+     view :by_person
+     view :by_outcome
+     view :by_person_and_outcome
+     view :by_updated_at
+     view :by_created_at
+  end
 
 end
